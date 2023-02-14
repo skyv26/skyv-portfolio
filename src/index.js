aaecdfb7ba22adbe856c0df2a1d8e754a50e2fda
skyv26 <dev.aakashv@gmail.com>
2023-02-14 22:24:26 +0530

    Mount the `App` on index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
+// import { HelmetProvider, Helmet } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <></>,
+  <App />,
 );
